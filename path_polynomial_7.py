d93bcb153be841596e9f068076bdeca425eefc2e
Eric Schneider <franz.eric.schneider@gmail.com>
2015-12-06 17:12:13 -0500

    poly7 path planning done

--- path_polynomial_7.py
+++ path_polynomial_7.py
@@ -1,3 +1,4 @@
+import rospy
 import numpy as np
 from matplotlib import pyplot
 
@@ -6,21 +7,23 @@ from ur5_model.msg import JointAngles
 
 
 def main():
+    rospy.init_node('PathPolynomial7')
     P7 = PathPolynomial7()
-    tf = 1.0
-    coefficients = P7.calculate_coefficients(0, 0, 0, 0,
-                                             35, 0, 0, 0, tf)
-    t = np.arange(0, tf, 0.01)
-    q = P7.angle_equation(coefficients, t)
-    Dq = P7.velocity_equation(coefficients, t)
-    DDq = P7.acceleration_equation(coefficients, t)
-    DDDq = P7.jerk_equation(coefficients, t)
-    print(type(DDDq))
-    pyplot.plot(t, q, color='b')
-    pyplot.plot(t, Dq/2.0, color='g')
-    pyplot.plot(t, DDq/10.0, color='k')
-    pyplot.plot(t, DDDq/100.0, color='r')
-    pyplot.show()
+    # Let the ROS node start up
+    rospy.sleep(1.0)
+    tf = 2.0
+    dt = 0.05
+    pose1 = JointAngles([0, 1.8, -0.46, 0, 0, 0])
+    pose2 = JointAngles([-0.5, 1, -1, 0, -2, -4])
+    pose3 = JointAngles([-3, 0.5, -0.5, -2, -2, -4])
+    pose4 = JointAngles([-2, 0.5, -2, 1, 0, -4])
+    P7.calculate_and_publish_end_stop_path(pose1, pose2, dt, tf)
+    rospy.sleep(0.5)
+    P7.calculate_and_publish_end_stop_path(pose2, pose3, dt, tf)
+    rospy.sleep(0.5)
+    P7.calculate_and_publish_end_stop_path(pose3, pose4, dt, tf)
+    rospy.sleep(0.5)
+    P7.calculate_and_publish_end_stop_path(pose4, pose1, dt, tf)
 
 
 class PathPolynomial7():
@@ -30,6 +33,45 @@ class PathPolynomial7():
         # There are 8 coefficients in a 7-degree polynomial, and there is a 7-degree
         # polynomial for every joint
         self.coefficients = [np.arange(8) for i in range(6)]
+        self.joint_pub = rospy.Publisher('/ur5_model/joint_command', JointAngles, queue_size=10)
+
+    def calculate_and_publish_end_stop_path(self, pose1, pose2, dt, tf):
+        t = np.arange(0, tf, dt)
+        arm_coefficients = self.calculate_arm_coefficients_end_stops(pose1, pose2, tf)
+        arm_angle_path = self.calculate_arm_angles(arm_coefficients, t)
+        self.publish_arm_path(arm_angle_path, dt, tf)
+
+    def calculate_arm_coefficients_end_stops(self, pose1, pose2, tf):
+        """
+        Takes the start and poses of arm motion, and time to complete path, and
+        returns 7-poly coefficients for a path
+        """
+        # Turn the JointAngles variables into numpy arrays
+        pose1 = np.array(pose1.arm_angle)
+        pose2 = np.array(pose2.arm_angle)
+        # Loop through the arm joints calculate coefficients for each joint
+        for i in range(6):
+            # Calculates the coefficients assuming 0 velocity, acceleration, and jerk at endpoints
+            self.coefficients[i] = self.calculate_coefficients(pose1[i], 0, 0, 0,
+                                                               pose2[i], 0, 0, 0, tf)
+        return(self.coefficients)
+
+    def calculate_arm_angles(self, cf, t):
+        """ Given coefficients and time vector, calculate paths for all arm angles """
+        angles = [np.arange(len(t)) for i in range(6)]
+        for i in range(6):
+            angles[i] = self.angle_equation(cf[i], t)
+        return(angles)
+
+    def publish_arm_path(self, ang, dt, tf):
+        """ Given angles and a time step, will try to publish the arm positions in time """
+        counter = 0.0
+        for i in range(len(ang[0])):
+            joint = JointAngles([ang[0][i], ang[1][i], ang[2][i], ang[3][i], ang[4][i], ang[5][i]])
+            self.joint_pub.publish(joint)
+            rospy.loginfo('Publishing, time elapsed = %.2f / %.2f', counter * dt, tf)
+            rospy.sleep(dt)
+            counter += 1
 
     def calculate_coefficients(self, q0, Dq0, DDq0, DDDq0, qf, Dqf, DDqf, DDDqf, tf):
         """
